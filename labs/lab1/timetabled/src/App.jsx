a62aa21fdd1986a2c33ab0e923b7a769834edbbb
Nasimul Labib <nlabib@students.kennesaw.edu>
2025-06-04 20:15:11 -0400

    lab1 basic started

---
+++ labs/lab1/timetabled/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  )
+}
+
+export default App
